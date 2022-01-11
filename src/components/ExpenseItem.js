3be1e88ec09c97756181484b9a2d9fe6e22d2bcc
Mustafa Qureshi <mustafashakeel@gmail.com>
2022-01-11 12:41:25 -0800

    updated

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 const ExpenseItem = (props) => {
+    const { dispatch } = useContext(AppContext);
+    const handleDeleteExpense = () => {
+        dispatch({
+            type: "DELETE_EXPENSE",
+            payload: props.id
+        });
+    }
     return (
         <li className="list-group-item d-flex justify-content-between ">
             {props.name}
             <span > {props.cost}</span>
+
+            <span onCLick={handleDeleteExpense} >Delete </span>
 
         </li>
     );
